bdf47f89939e72a0a936cea25c263d73013ef967
Chayma Guerrassi <Chaymaa.guer@gmail.com>
2020-02-12 14:26:57 +0100

    i

--- TP/TP3/main.c
+++ TP/TP3/main.c
@@ -2,6 +2,42 @@
 
 typedef struct timeval Time;
 
+
+int nb_ms_vers_centiemes(int nb_ms);
+int nb_ms_vers_secondes(int nb_ms);
+int nb_ms_vers_minutes(int nb_ms);
+int nb_ms_vers_heures(int nb_ms);
+
+
+int main(int argc, const char * argv[]) {
+    
+    int cent = nb_ms_vers_centiemes(1213);
+    printf("cent = %d", cent);
+}
+
+
 long int intervalle_ms(Time debut, Time fin) {
     return (fin.tv_sec - debut.tv_sec) * 1000 + (fin.tv_usec - debut.tv_usec) / 1000;
 }
+
+int nb_ms_vers_centiemes(int nb_ms){
+    
+    return (nb_ms/10)%100;
+}
+
+
+int nb_ms_vers_secondes(int nb_ms){
+    
+    return (nb_ms/1000)%60;
+}
+
+int nb_ms_vers_minutes(int nb_ms){
+    
+    return (nb_ms/(60*1000))%60;
+}
+
+int nb_ms_vers_heures(int nb_ms){
+    
+    return nb_ms_vers_minutes(nb_ms)/60;
+}
+
